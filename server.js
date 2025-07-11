2542f9175a02f5267daf37dfc4df4f44aa1cf641
SIVAKUMAR S <122508923+SIVA2118@users.noreply.github.com>
2025-07-11 13:47:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // MongoDB Connection using .env
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
